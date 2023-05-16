005a0049f1abd1b570af78e201ff851ba4fb0e76
durga <durgaprasad230698@gmail.com>
2023-05-16 17:07:47 +0530

    todo changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const TodoList = ({todolist, deleteHandler}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {todolist.map((todo,index) =>
+      <div key={index}>
+        <h5>{todo} <button onClick={() => deleteHandler(index)}>Delete</button></h5>
+        </div>)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default TodoList
